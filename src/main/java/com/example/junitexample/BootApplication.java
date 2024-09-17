4ee41d419b746675bf2abe88cd268088d494dd81
Jong1 <whddnjs822@gmail.com>
2024-09-17 14:55:17 +0900

    style : 파일명, 확장자 변경

--- src/main/java/com/example/junitexample/BootApplication.java
+++ src/main/java/com/example/junitexample/BootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JunitExampleApplication {
+public class BootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JunitExampleApplication.class, args);
+        SpringApplication.run(BootApplication.class, args);
     }
 
 }
