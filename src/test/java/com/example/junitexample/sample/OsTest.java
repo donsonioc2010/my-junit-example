88dc9efd079eae56bcde8c9595a47a9ba5cc3fd6
Jong1 <whddnjs822@gmail.com>
2024-09-19 21:06:04 +0900

    test : 예제용 테스트 케이스 제작
    - OS에 따른 테스트케이스 제작
    - 타임아웃을 활용한 테스트케이스 제작

---
+++ src/test/java/com/example/junitexample/sample/OsTest.java
@@ -0,0 +1,37 @@
+package com.example.junitexample.sample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+@Slf4j
+@DisplayName("각 OS환경에 맞는 테스트 지원")
+public class OsTest {
+    @Test
+    @EnabledOnOs(OS.MAC)
+    @DisplayName("Mac인 경우에만 실행됩니다.")
+    void whenRunningOsIsMac_thenTestCaseIsSuccess() {
+        String osName = System.getProperty("os.name");
+        log.info("현재 실행 된 테스트케이스는 MacOS버전이며, OS정보 >>> {}",  osName);
+        Assertions.assertTrue(osName.contains("Mac OS"));
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    @DisplayName("윈도우에서만 실행되는 테스트")
+    void whenRunningOsIsWindows_thenTestCaseIsSuccess() {
+        log.info("현재 실행환경은 윈도우~");
+        Assertions.assertTrue(System.getProperty("os.name").contains("Windows"));
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    @DisplayName("윈도우에서만 실행되는 테스트")
+    void whenRunningOsIsLinux_thenTestCaseIsSuccess() {
+        log.info("현재 실행환경은 리눅스~");
+        Assertions.assertTrue(System.getProperty("os.name").contains("Linux"));
+    }
+}
---
+++ src/test/java/com/example/junitexample/sample/TimeOutTest.java
@@ -0,0 +1,51 @@
+package com.example.junitexample.sample;
+
+import java.time.Duration;
+import java.util.stream.IntStream;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+@DisplayName("TimeOut Test Samples")
+public class TimeOutTest {
+    @Test
+    @DisplayName("1초 이내로 실행되면 테스트 케이스는 성공한다.")
+    void whenIn1Sec_thenTestCaseSuccess() {
+        Assertions.assertTimeout(Duration.ofSeconds(1L), () -> {
+            long startTime = System.currentTimeMillis();
+
+            log.info("타임아웃 성공케이스 로직 실행");
+            int zeroOneHundred = IntStream.range(0, 100).sum();
+            log.info("0 ~ 100 합은? >>> {}", zeroOneHundred);
+            long totalTime = System.currentTimeMillis() - startTime;
+
+            log.info("타임아웃 성공케이스 종료~, Total 소요시간 >>> {} millSeconds", Duration.ofSeconds(totalTime).getSeconds());
+
+        });
+    }
+
+    @Test
+    @DisplayName("1초 이외로 실행되면 테스트 케이스는 성공한다.")
+    void whenOut1Sec_thenTestCaseSuccess() {
+        Assertions.assertThrows(AssertionError.class, () -> {
+            Assertions.assertTimeout(Duration.ofSeconds(1L), () -> {
+                long startTime = System.currentTimeMillis();
+                log.info("타임아웃 실패케이스 로직 실행");
+                sleep(1500L);
+                long totalTime = System.currentTimeMillis() - startTime;
+
+                log.info("타임아웃 실패케이스 종료~, Total 소요시간 >>> {} millSeconds", Duration.ofSeconds(totalTime).getSeconds());
+            });
+        },  "1초를 초과했기 때문에 AssertionError가 발생해야 함");
+    }
+
+    private void sleep(long mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
