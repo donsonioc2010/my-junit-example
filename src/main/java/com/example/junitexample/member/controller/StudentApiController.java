754b36bda4884335556fd926869df016a34b293f
Jong1 <whddnjs822@gmail.com>
2024-09-20 11:50:39 +0900

    feat : 회원관리, 강좌의 구조 추가

---
+++ src/main/java/com/example/junitexample/member/controller/StudentApiController.java
@@ -0,0 +1,11 @@
+package com.example.junitexample.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+// TODO : Spring Web 추가하게 되면 이후에 @RestController 추가
+public class StudentApiController {
+
+}
